31dd98259f499388b102cccb6a14be988fc1ddf1
Palina Bohush <palinabohush@gmail.com>
2025-02-17 14:56:22 +0300

    View for events in process

--- events/views.py
+++ events/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render, redirect
 from .models import Articles
 from .forms import ArticleForm
+from django import DetailView
 
 def events_home(request):
     events = Articles.objects.order_by('published_date')
     return render(request, 'events/events_home.html', {'events': events})
+
+class EventDetailView(DetailView):
+    model = Articles
+    template_name = 'events/detail_view.html'
+    context_object_name = 'event'
 
 def create_event(request):
     error = ''
